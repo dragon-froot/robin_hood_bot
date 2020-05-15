bc44a4090acccbc243077dc96fd2ab0006b89aa2
Mason <dragon-froot710@protonmain.com>
2020-05-15 01:14:51 -0700

    the script now returns the symbol and av_price of current holdings

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 import requests 
 import json
-import robin_stocks as r 
+import robin_stocks as r
+import sys
 
+
+# While the program is running this makes sure that the user is logged in.
 # Returns the access token as a string
 def login():
     login = r.login('dragon-froot710@protonmail.com', '1185949Bobsaget1')
     return login['access_token']
+
     # print(login['access_token'])
 
 # Gets a list of all the stocks you own and returns an array of url's to get other data 
-def get_list_of_owned_stocks():
-    data = r.account.get_current_positions(info=None)
-    for i in data:
-        url = i['url']
-        head = {"Accept": "application/json"}
-        payload = {'name': 'Technology', "Authorizaton": "Token " + login()}
-        # res = requests.get(url, headers=head, auth=payload, authori)
-        print(i)
-        # print(res.text)
+def owned_stocks():
+    data = r.get_current_positions()
+    length = len(data)
+    #returns a list of the symbols of my current holdings
+    for item in data:
+        item['symbol'] = r.get_symbol_by_url(item['instrument'])
+        holding = {
+            "price": item['average_buy_price'],
+            "symbol": item['symbol']
+        }
+        print(holding)
+
+def getCurrentPrices(sym):
+    for i in sym:
+        return i['price']
+        
+        
+
+
+        
 
     
  
-login()
-get_list_of_owned_stocks()
+# login()
+# get_list_of_owned_stocks()
+hold = ['GOOGL', 'WORK']
+if __name__ == "__main__":
+    
+    login()
+    owned_stocks()
+
+        # hold will be the return value of owned_stocks
+        
+
 
 # Make sure that the user logs in from the terminal
+# Have the owned_stocks return a list of our current holdings for the inverstopia api
