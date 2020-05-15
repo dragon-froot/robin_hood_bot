79eb5a64f9926c8f60a192a681b4ae6b3106bbd0
Mason <dragon-froot710@protonmain.com>
2020-05-15 14:01:42 -0700

    bot now returns Needed info for current holdings

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ password = parser.add_argument("-p", "--password",
                             help="This is where you will include your password")
 
 # While the program is running this makes sure that the user is logged in.
-# Returns the access token as a string
 def login():
-    # login = r.login('dragon-froot710@protonmail.com', '1185949Bobsaget1')
+    
     login = r.login(email, password)
+    #return the account access token for future requests
     return login['access_token']
 
-    # print(login['access_token'])
 
 # Gets a list of all the stocks you own and returns an array of url's to get other data 
 def owned_stocks():
@@ -29,37 +28,35 @@ def owned_stocks():
     #returns a list of the symbols of my current holdings
     for item in data:
         item['symbol'] = r.get_symbol_by_url(item['instrument'])
+
+
+        current = getCurrentPrices(item['symbol'])
         holding = {
-            "price": item['average_buy_price'],
-            "symbol": item['symbol']
+            "buy_price": item['average_buy_price'],
+            "symbol": item['symbol'],
+            "quantity": item['average_buy_price'],
+            "current_price": current['ask_price']
         }
-        getCurrentPrices(item['symbol'])
+        
+        print(holding)
 
 
 
 
 def getCurrentPrices(sym):
-    req = requests.get('https://robinhood.com/stocks/{0}', sym)
-    print(req.data)
-        
-        
-
-
-        
-
+    data = r.get_quotes(sym)
     
- 
-# login()
-# get_list_of_owned_stocks()
-hold = ['GOOGL', 'WORK']
+    for i in data:
+        return i
+        
+        
+
+
+
 if __name__ == "__main__":
     
     login()
     owned_stocks()
-
-        # hold will be the return value of owned_stocks
-        
+    
 
 
-# Make sure that the user logs in from the terminal
-# Have the owned_stocks return a list of our current holdings for the inverstopia api
