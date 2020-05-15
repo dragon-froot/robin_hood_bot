b59a43f40c124c8fbfb01dcfa6657952f2f3243f
Mason <dragon-froot710@protonmain.com>
2020-05-15 01:42:36 -0700

    Returns list of owned stocks and associated info

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import requests
 import json
 import robin_stocks as r
 import sys
+from argparse import ArgumentParser
 
+# THis is where the script will get the user credentials
+parser = ArgumentParser(description="You will include the email and password to login to this")
+email = parser.add_argument("-e", "--email",
+                            action="store_true", dest="rank", default=False,
+                            help="This is where you will include your email ")
+password = parser.add_argument("-p", "--password",
+                            action="store_true", dest="rank", default=False,
+                            help="This is where you will include your password")
 
 # While the program is running this makes sure that the user is logged in.
 # Returns the access token as a string
 def login():
-    login = r.login('dragon-froot710@protonmail.com', '1185949Bobsaget1')
+    # login = r.login('dragon-froot710@protonmail.com', '1185949Bobsaget1')
+    login = r.login(email, password)
     return login['access_token']
 
     # print(login['access_token'])
@@ -23,11 +33,14 @@ def owned_stocks():
             "price": item['average_buy_price'],
             "symbol": item['symbol']
         }
-        print(holding)
+        getCurrentPrices(item['symbol'])
+
+
+
 
 def getCurrentPrices(sym):
-    for i in sym:
-        return i['price']
+    req = requests.get('https://robinhood.com/stocks/{0}', sym)
+    print(req.data)
         
         
 
